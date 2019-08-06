a5f37144b87549979bc53c69671135291e93112c
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-06 10:07:02 +0100

    Update test1.js
    Test case number 1,  Test case 1: Automate Form Authentication
    • Scenario 1: Try to login with correct username and wrong password and assert login validation
    • Scenario 2: Try to login with incorrect username and correct password and assert login validation
    • Scenario 3: Try to login with correct username and password and then logout

--- tests/test1.js
+++ tests/test1.js
@@ -10,6 +10,12 @@ suite(function(env) {
             this.timeout(10000);
         let driver;
         let page;
+        let username = "tomsmith";
+        let password = "SuperSecretPassword!";           //I will use this variables mostly as an input or to evaluate the test results
+        let loginInvalidUsername = "Your username is invalid!";
+        let loginInvalidPassword = "Your password is invalid!";
+        let loginSuccess = "You logged into a secure area!";
+        let logoutSuccess = "You logged out of the secure area!";
 
         before(async function() {
             driver = await env.builder().build();
@@ -17,20 +23,43 @@ suite(function(env) {
             await page.open();
         });
 
-
+//correct username wrong password
                 it('scenario1', async function() {
-
+                  await driver.findElement(page.locators.username).click();
+                  await driver.findElement(page.locators.username).sendKeys(username);
+                  await driver.findElement(page.locators.password).click();
+                  await driver.findElement(page.locators.password).sendKeys("oops wrong password");
+                  await page.Login();
+                  let infomessage = await driver.findElement(page.locators.message);
+                  let text = await infomessage.getText();
+                  assert(text.includes(loginInvalidPassword));
                 });
-
+//wrong username correct password
                 it('scenario2', async function() {
-
+                  await driver.findElement(page.locators.username).click();
+                  await driver.findElement(page.locators.username).sendKeys("oops wrong username");
+                  await driver.findElement(page.locators.password).click();
+                  await driver.findElement(page.locators.password).sendKeys(password);
+                  await page.Login();
+                  let infomessage = await driver.findElement(page.locators.message);
+                  let text = await infomessage.getText();
+                  assert(text.includes(loginInvalidUsername));
                 });
-
+//successfull login well done buddy
                 it('scenario3', async function() {
-
+                  await driver.findElement(page.locators.username).click();
+                  await driver.findElement(page.locators.username).sendKeys(username);
+                  await driver.findElement(page.locators.password).click();
+                  await driver.findElement(page.locators.password).sendKeys(password);
+                  await page.Login();
+                  let infomessage = await driver.findElement(page.locators.message);
+                  let text = await infomessage.getText();
+                  assert(text.includes(loginSuccess));
+                  await page.Logout();
+                  let infomessage2 = await driver.findElement(page.locators.message);
+                  let text2 = await infomessage2.getText();
+                  assert(text2.includes(logoutSuccess));
                 });
-
-
 
 
         after(async function() {
