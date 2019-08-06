bac8e433ca03b5d8ea2b1e09a3a35fe58f901d67
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-06 10:07:27 +0100

    Update page1.js
    Test case number 1

--- pages/page1.js
+++ pages/page1.js
@@ -6,15 +6,26 @@ class FormAuth {
   constructor(driver){
     this.driver = driver;
     this.locators = {
-
+      message : By.id('flash'),
+      logout : By.css('#content > div > a'),
+      username : By.css('#username'),
+      password : By.css('#password'),
+      login : By.css('#login > button')
     }
   }
 
   open(){
     this.driver.get(url);
   }
+  async Login() {
+      await this.driver.findElement(this.locators.login).click();
+  }
+  async Logout() {
+      await this.driver.findElement(this.locators.logout).click();
+  }
 }
 
 
 
 module.exports = FormAuth;
+
